2b8dc20ae7793fb85bbfc32d789da9a3d75a8d06
Sirishavulasi1706 <sirishanaidu1706@gmail.com>
2026-07-04 12:40:19 +0530

    Configure PostgreSQL and create User model

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "this_is_a_secret_key"
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
